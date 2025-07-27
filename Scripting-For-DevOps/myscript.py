c85664bc1c0f737f4a8a5df78865e8e61667ec22
Abhishek Prasad <121235088+Abhishek72-avail@users.noreply.github.com>
2025-07-27 08:42:13 +0000

    add this file

---
+++ Scripting-For-DevOps/myscript.py
@@ -0,0 +1,8 @@
+#!/bin/usr/env python3
+print(''This is our Python Script');
+print('We are running Hello World');
+print("conclusion");
+print("Python powers over 70% of DevOps automation tasks due to its simplicity, flexibility, and rich ecosystem. From scripting CI/CD pipelines to managing cloud infrastructure, it streamlines workflows across environments. With libraries like Boto3, Ansible, and Fabric, Python remains a top choice for scalable and efficient DevOps automation.To run the python script on Ubuntu Machine is a striaghtforward process. By understanding how to run Python scripts, you can begin exploring the powerful tools Python offers, including those essential for AI development.");
+
+
+# Python powers over 70% of DevOps automation tasks due to its simplicity, flexibility, and rich ecosystem. From scripting CI/CD pipelines to managing cloud infrastructure, it streamlines workflows across environments. With libraries like Boto3, Ansible, and Fabric, Python remains a top choice for scalable and efficient DevOps automation.To run the python script on Ubuntu Machine is a striaghtforward process. By understanding how to run Python scripts, you can begin exploring the powerful tools Python offers, including those essential for AI development.
---
+++ Scripting-For-DevOps/simpleline.py
@@ -0,0 +1,4 @@
+print('This is our Python Script');
+print('We are Running Hello World');
+print("conclusion");
+print("Python powers over 70% of DevOps automation tasks due to its simplicity, flexibility, and rich ecosystem. From scripting CI/CD pipelines to managing cloud infrastructure, it streamlines workflows across environments. With libraries like Boto3, Ansible, and Fabric, Python remains a top choice for scalable and efficient DevOps automation.To run the python script on Ubuntu Machine is a striaghtforward process. By understanding how to run Python scripts, you can begin exploring the powerful tools Python offers, including those essential for AI development.");
